dab95b296167d7749b8ccf3c4082eba0f0bb3f35
Hedi Lengliz <lengliz01hedi@gmail.com>
2025-01-16 12:02:31 +0100

    Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   app: {
-    baseURL: '/<HediLengliz.github.io>/', // GitHub Pages base URL
+    baseURL: '/', // GitHub Pages base URL
   }
 })
